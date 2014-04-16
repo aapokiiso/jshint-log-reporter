21a6a0ed967a5642b7322304358946fa6e8a7496
Aapo Kiiso <aapo@kii.so>
2014-04-16 15:19:59 +0300

    Added necessary files, initialization.

---
+++ index.js
@@ -0,0 +1,34 @@
+/* ----------------------------- */
+/*    ~ JSHint error logger ~    */
+/* ----------------------------- */
+
+/**
+*	This module takes JSHint errors, formats them and saves them to a user-defined filepath.
+*/
+
+/* ------------------ */
+/*    Dependencies    */
+/* ------------------ */
+
+/* --------------- */
+/*    Functions    */
+/* --------------- */
+
+/**
+*	function reporter(Array [errors])
+*		Loop through errors, format them and  write to file.
+*
+*		[errors]: list of errors JSHint has generated.
+*/
+
+function reporter(errors){
+
+}
+
+/* ------------- */
+/*    Exports    */
+/* ------------- */
+
+module.exports = {
+	reporter: reporter
+};
